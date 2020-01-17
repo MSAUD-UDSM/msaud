da56457e5cd47298e1431d8ababccf9c75d8c486
unknown <msaud.udsm@gmail.com>
2020-01-17 19:25:39 +0300

    df

--- contact.php
+++ contact.php
@@ -6,17 +6,16 @@ $lname = $_POST['lname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $msg = $_POST['msg'];
-$output = "Name: " . $fname. "\n\n\nMessage: " . $msg . "\n\nPhone: ".$phone;
+$output = "Name: " . $fname . "\nPhone: " . $phone . "\n\nMessage: " . $msg;
 
 $to = 'msaud.udsm@gmail.com';
 $headers = 'FROM: ' . $email . '';
 
 $send = mail($to, $fname, $output . "\n\n***This message has been sent from MSAUD Website ", $headers);
 
-if ($send)
-{
+if ($send) {
+
     echo "successfully";
 } else {
     echo "still loading have patience........";
 }
-?>
