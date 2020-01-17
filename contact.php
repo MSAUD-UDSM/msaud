3025f02be70f0cb3f460db78aeb30d286e865331
unknown <msaud.udsm@gmail.com>
2020-01-17 15:33:47 +0300

    added php file

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+// //Taking all values
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+$output = "Name: " . $fname. "\n\n\nMessage: " . $msg . "\n\nPhone: ".$phone;
+
+$to = 'msaud.udsm@gmail.com';
+$headers = 'FROM: ' . $email . '';
+
+$send = mail($to, $fname, $output . "\n\n***This message has been sent from MSAUD Website ", $headers);
+
+if ($send)
+{
+    echo "successfully";
+} else {
+    echo "still loading have patience........";
+}
+?>
